fb68692e232cd164a5e6d495706aecf744705331
Maximilian <max-dz@hotmail.com>
2018-09-14 16:22:07 +0200

    Programmet klarar nu att ta emot string och printa ut Sudukobräde.

--- SudukoSolver/SudukoSolver/SudukoMethods.cs
+++ SudukoSolver/SudukoSolver/SudukoMethods.cs
@@ -21,23 +21,29 @@ namespace SudukoSolver
             
             for (int row = 0; row < puzzle.GetLength(0); row++)
             {
+                Console.WriteLine("+-----------+-----------+-----------+");
+                Console.Write("| ");
                 for (int col = 0; col < puzzle.GetLength(1); col++)
                 {
+                   
                     if (text[index].Equals('0'))
                     {
                         puzzle[row, col] = '-';
+                        Console.Write(puzzle[row,col] + " | ");
                     }
-
+                    
                     else
                     {
                         puzzle[row, col] = text[index];
+                        Console.Write(puzzle[row, col] + " | ");
                     }
-
                     index++;
                 }
+                Console.WriteLine();
             }
+            Console.WriteLine("+-----------+-----------+-----------+");
 
-            
+
         }
     }
 }
