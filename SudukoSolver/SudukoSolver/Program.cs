d28af83cac778b126139f22ba002f3f1cb749bf0
Maximilian <max-dz@hotmail.com>
2018-09-14 12:17:33 +0200

    Test

---
+++ SudukoSolver/SudukoSolver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudukoSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Testar om det fungerar. Vänligen bekräfta med en CW ""JA""");
+        }
+    }
+}
