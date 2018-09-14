3c3717dace7d4f401ad042a615cd5a547bb85110
Robert Tadjer <robert.tadjer@yh.nackademin.se>
2018-09-14 15:37:48 +0200

    Början på Sudoku.

--- SudukoSolver/SudukoSolver/Program.cs
+++ SudukoSolver/SudukoSolver/Program.cs
@@ -8,9 +8,51 @@ namespace SudukoSolver
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(@"Testar om det fungerar. Vänligen bekräfta med en CW ""JA""");
+        
+            {
+
+                public set puzzle (string text) text = "619030040270061008000047621486302079000014580031009060005720806320106057160400030"
+         
+
+                public char[] Print Puzzle(string text)
+                { 
+                    int index = 0; 
+
+                    for (int row < puzzle.UpperBounds(), col++)
+                    {
+
+                        if (text)[index].Equals("0");
+                    {
+
+                        puzzle(col, row) = "-";
+                    }
+
+                    else 
+                    {
+                        puzzle(col, row) = text[index];
+                    }
+                  
+                    index++;
+
+                    }
+
+                    for (int col < puzzle.UpperBounds(), col++)
+                    {
+
+                    if (text)[index].Equals("0");
+                    {
+                    puzzle(col, row) = "-";
+                    }
+                    
+                    else
+                    {
+                        puzzle
+                }
+            }
+
+
         }
+
     }
 }
+
