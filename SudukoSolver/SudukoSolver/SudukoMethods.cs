cc504287af86faa45fe16851d3102aad68d1b8fd
Maximilian <max-dz@hotmail.com>
2018-09-18 17:57:50 +0200

    Added RecusionSolver and cleaned up some code. More cleaning inc.

--- SudukoSolver/SudukoSolver/SudukoMethods.cs
+++ SudukoSolver/SudukoSolver/SudukoMethods.cs
@@ -38,12 +38,10 @@ namespace SudukoSolver
             Console.WriteLine("+-----------+-----------+-----------+");
         }
 
-        /**
-         * Prints the selected puzzle.
-         */
+        /* Prints the selected puzzle.*/
         private void PrintPuzzle(char[,] puzzle)
         {
-            for(int rows = 0; rows <= puzzle.GetUpperBound(0); rows++)
+            for (int rows = 0; rows <= puzzle.GetUpperBound(0); rows++)
             {
                 Console.WriteLine("+-----------+-----------+-----------+");
                 Console.Write("| ");
@@ -56,14 +54,12 @@ namespace SudukoSolver
             Console.WriteLine("+-----------+-----------+-----------+");
         }
 
-        /**
-         * Returnerar true/false om talet finns i raden.
-         */
+        /*Returnerar true/false om talet finns i raden.*/
         private bool FindInRow(char number, int row)
         {
             for (int col = 0; col <= puzzle.GetUpperBound(1); col++)
             {
-                if (number == puzzle[row,col])
+                if (number == puzzle[row, col])
                 {
                     return true;
                 }
@@ -71,24 +67,21 @@ namespace SudukoSolver
             return false;
         }
 
-        /**
-         * Returnerar true/false om talet finns i kolumnen.
-         */
+        /* Returnerar true/false om talet finns i kolumnen.*/
         private bool FindInCol(char number, int col)
         {
             for (int row = 0; row <= puzzle.GetUpperBound(0); row++)
             {
-                if (number == puzzle[row,col])
+                if (number == puzzle[row, col])
                 {
                     return true;
                 }
             }
+
             return false;
         }
-
-        /**
-         * Kontrollerar om sudokut är klart.
-         */
+        
+        /*Kontrollerar om sudokut är klart.*/
         private bool CheckIfComplete()
         {
             int totalRow;
@@ -97,27 +90,23 @@ namespace SudukoSolver
                 totalRow = 0;
                 for (int col = 0; col < puzzle.GetLength(1); col++)
                 {
-                    totalRow += (int)Char.GetNumericValue(puzzle[row, col]);
-                    
+                    totalRow += (int) Char.GetNumericValue(puzzle[row, col]);
                 }
                 if (totalRow != 45)
                 {
                     return false;
                 }
-                
             }
             return true;
         }
-
-        /**
-         * Collects the numbers in the selected 3x3 area.
-         */
+        
+        /* Collects the numbers in the selected 3x3 area.*/
         private List<char> FindNumbers(int startY, int stopY, int startX, int stopX)
         {
             List<char> boxContains = new List<char>();
-            for(int rows = startY; rows < stopY; rows++)
+            for (int rows = startY; rows < stopY; rows++)
             {
-                for(int cols = startX; cols < stopX; cols++)
+                for (int cols = startX; cols < stopX; cols++)
                 {
                     if (!puzzle[rows, cols].Equals('-'))
                     {
@@ -125,16 +114,15 @@ namespace SudukoSolver
                     }
                 }
             }
+
             return boxContains;
         }
 
 
-        /**
-         * Returnerar en lista med siffror som redan finns i boxen.
-         */
+        /*Returnerar en lista med siffror som redan finns i boxen.*/
         private List<char> GetNumbersInBox(int y, int x)
         {
-            List <char> numbersInBox = new List<char>();
+            List<char> numbersInBox = new List<char>();
             int boxLength = 3;
             int endValue = puzzle.GetLength(0) / boxLength;
             int startY = y / boxLength;
@@ -145,9 +133,7 @@ namespace SudukoSolver
             return numbersInBox;
         }
 
-        /**
-         * Returnerar en lista med alla tal som skulle kunna vara i positionen.
-         */
+        /*Returnerar en lista med alla tal som skulle kunna vara i positionen.*/
         private List<char> GetInputNumbers(int y, int x)
         {
             List<char> inputNumbers = new List<char>();
@@ -156,21 +142,20 @@ namespace SudukoSolver
             for (int testValue = 1; testValue < 10; testValue++)
             {
                 string tempValue = testValue.ToString();
-                
+
                 if (!FindInRow(tempValue[0], y) && !FindInCol(tempValue[0], x) && !boxContains.Contains(tempValue[0]))
                 {
                     inputNumbers.Add(tempValue[0]);
                 }
             }
+
             return inputNumbers;
         }
 
-        /**
-         * Easy sudokusolver.
-         */
+        /*Easy sudokusolver.*/
         public void Solve()
         {
-            
+
             List<char> inputNumbers = new List<char>();
             bool puzzleNotSolved = true;
             while (puzzleNotSolved)
@@ -179,32 +164,78 @@ namespace SudukoSolver
                 if (CheckIfComplete())
                 {
                     PrintPuzzle(puzzle);
-                    Console.WriteLine("Solution found!");
+                    Console.WriteLine();
+                    Console.WriteLine("***********SOLUTION FOUND!***********");
                     break;
                 }
-                for(int rows = 0; rows <= puzzle.GetUpperBound(0); rows++)
+
+                for (int rows = 0; rows <= puzzle.GetUpperBound(0); rows++)
                 {
-                    for(int cols = 0; cols <= puzzle.GetUpperBound(1); cols++)
+                    for (int cols = 0; cols <= puzzle.GetUpperBound(1); cols++)
                     {
                         inputNumbers = GetInputNumbers(rows, cols);
-                        
+
                         if (puzzle[rows, cols].Equals('-') && (inputNumbers.Count == 1))
                         {
                             puzzle[rows, cols] = inputNumbers.ElementAt(0);
                             puzzleNotSolved = true;
                         }
-                       
+
                         inputNumbers.Clear();
                     }
                 }
             }
+
+            RecursionSolve();
         }
 
-        /**
-         * Solves hard sudoku puzzles using mutual recursion searching for a solution using depth-first.
-         */
+        private bool GuessNumber(int y, int x, char[,] puzzle)
+        {
+            List<char> inputNumber = new List<char>();
+
+            for (int testValue = 1; testValue < 10; testValue++)
+            {
+                inputNumber = GetInputNumbers(y, x);
+                string tempValue = testValue.ToString();
+                if (inputNumber.Contains(tempValue[0]))
+                {
+                    puzzle[y, x] = tempValue[0];
+
+                    if (RecursionSolve())
+                    {
+                        if (CheckIfComplete())
+                        {
+                            PrintPuzzle(puzzle);
+                            Console.WriteLine();
+                            Console.WriteLine("***********SOLUTION FOUND!***********");
+                            Console.ReadLine();
+                            Environment.Exit(0);
+                        }
+
+                        return true;
+                    }
+                }
+            }
+
+            puzzle[y, x] = '-';
+            return false;
+        }
+    
+
+            /*Solves hard sudoku puzzles using mutual recursion searching for a solution using depth-first.*/
          private bool RecursionSolve()
         {
+            for (int rows = 0; rows < puzzle.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < puzzle.GetLength(1); cols++)
+                {
+                    if (puzzle[rows,cols].Equals('-'))
+                    {
+                        return GuessNumber(rows,cols,puzzle);
+                    }
+                }
+            }
+            
             return true;
         }
     }
--- SudukoSolver/SudukoSolver/SuperEpicGame.cs
+++ SudukoSolver/SudukoSolver/SuperEpicGame.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
-namespace TestingWriteRead
+namespace SudukoSolver
 {
-    class Program
+    class Spel
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
